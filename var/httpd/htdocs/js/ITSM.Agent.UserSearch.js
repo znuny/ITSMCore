6a9cc581c369d343b7eafaca0fb59d89741a5f76
Denny Korsukéwitz <dk@znuny.com>
2023-02-02 19:34:36 +0100

    Added var/httpd/htdocs/js/ITSM.Agent.UserSearch.js - is used by: ITSMConfigurationManagement. Integration of Znuny4OTRS-ITSMConfigItemTypes into ITSMConfigurationManagement.
    Tidied sopm Intros.

---
+++ var/httpd/htdocs/js/ITSM.Agent.UserSearch.js
@@ -0,0 +1,80 @@
+// --
+// Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
+// Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+// --
+// This software comes with ABSOLUTELY NO WARRANTY. For details, see
+// the enclosed file COPYING for license information (GPL). If you
+// did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
+// --
+
+"use strict";
+
+var ITSM = ITSM || {};
+ITSM.Agent = ITSM.Agent || {};
+
+/**
+ * @namespace ITSM.Agent.UserSearch
+ * @memberof ITSM.Agent
+ * @author OTRS AG
+ * @description
+ *      This namespace contains the special module functions for the customer search.
+ */
+ITSM.Agent.UserSearch = (function (TargetNS) {
+
+    /**
+     * @name Init
+     * @memberof ITSM.Agent.UserSearch
+     * @function
+     * @param {jQueryObject} $Element - The jQuery object of the input field with autocomplete.
+     * @description
+     *      Initializes the special module functions.
+     */
+    TargetNS.Init = function ($Element) {
+
+        if (isJQueryObject($Element)) {
+
+            Core.UI.Autocomplete.Init($Element, function (Request, Response) {
+                    var URL = Core.Config.Get('Baselink'),
+                        Data = {
+                            Action: 'AgentUserSearch',
+                            Term: Request.term,
+                            MaxResults: Core.UI.Autocomplete.GetConfig('MaxResultsDisplayed')
+                        };
+
+                    $Element.data('AutoCompleteXHR', Core.AJAX.FunctionCall(URL, Data, function (Result) {
+                        var ValueData = [];
+                        $Element.removeData('AutoCompleteXHR');
+                        $.each(Result, function () {
+                            ValueData.push({
+                                label: this.UserValue + " (" + this.UserKey + ")",
+                                value: this.UserValue,
+                                key:   this.UserKey,
+                                show:  this.UserValue  //  todo denny delete?
+                            });
+                        });
+                        Response(ValueData);
+                    }));
+                },
+                function (Event, UI) {
+                    $Element.val(UI.item.show);
+
+                    // set hidden field SelectedCustomerUser
+                    // escape possible colons (:) in element id because jQuery can not handle it in id attribute selectors
+                    $('#' + Core.App.EscapeSelector($Element.attr('id')) + 'Selected').val(UI.item.value);
+
+                    return false;
+                },
+                'UserSearch'
+            );
+        }
+
+        // before unload remove old selected data. If the page is reloaded (with F5) this data stays in the field and invokes an ajax request otherwise
+        $(window).on('beforeunload.UserSearch', function () {
+            // escape possible colons (:) in element id because jQuery can not handle it in id attribute selectors
+            $('#' + Core.App.EscapeSelector($Element.attr('id')) + 'Selected').val('');
+            return;
+        });
+    };
+
+    return TargetNS;
+}(ITSM.Agent.UserSearch || {}));
